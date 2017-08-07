b181f51d424a09f9991ba1128f986cca45801cec
JustinHtay <justinhtay@gmail.com>
2017-08-06 20:38:19 -0400

    used matlab to parse the data

---
+++ jsonlab-1.1/edges.js
@@ -0,0 +1,1207 @@
+var edges = 
+	[
+		{
+			"from": "ECE 3085",
+			"to": "ECE 4551"
+		},
+		{
+			"from": "ECE 2020",
+			"to": "ECE 2036"
+		},
+		{
+			"from": "ECE 2030",
+			"to": "ECE 2036"
+		},
+		{
+			"from": "ECE 2026",
+			"to": "ECE 2036"
+		},
+		{
+			"from": "ECE 2025",
+			"to": "ECE 2036"
+		},
+		{
+			"from": "ECE 2020",
+			"to": "ECE 2035"
+		},
+		{
+			"from": "ECE 2030",
+			"to": "ECE 2035"
+		},
+		{
+			"from": "ECE 2020",
+			"to": "ECE 3077"
+		},
+		{
+			"from": "ECE 2030",
+			"to": "ECE 3077"
+		},
+		{
+			"from": "ECE 2040",
+			"to": "ECE 3077"
+		},
+		{
+			"from": "ECE 3075",
+			"to": "ECE 6601"
+		},
+		{
+			"from": "ECE 6601",
+			"to": "ECE 6602"
+		},
+		{
+			"from": "ECE 2020",
+			"to": "ECE 2031"
+		},
+		{
+			"from": "ECE 2030",
+			"to": "ECE 2031"
+		},
+		{
+			"from": "ECE 2035",
+			"to": "ECE 2031"
+		},
+		{
+			"from": "ECE 2036",
+			"to": "ECE 2031"
+		},
+		{
+			"from": "ECE 8750",
+			"to": "ECE 8751"
+		},
+		{
+			"from": "ECE 6250",
+			"to": "ECE 7252"
+		},
+		{
+			"from": "ECE 6601",
+			"to": "ECE 7251"
+		},
+		{
+			"from": "ECE 6100",
+			"to": "ECE 7142"
+		},
+		{
+			"from": "ECE 6140",
+			"to": "ECE 7141"
+		},
+		{
+			"from": "ECE 6130",
+			"to": "ECE 7131"
+		},
+		{
+			"from": "ECE 6100",
+			"to": "ECE 7103"
+		},
+		{
+			"from": "ECE 6100",
+			"to": "ECE 7102"
+		},
+		{
+			"from": "ECE 2025",
+			"to": "ECE 6790"
+		},
+		{
+			"from": "ECE 3075",
+			"to": "ECE 6790"
+		},
+		{
+			"from": "ECE 4784",
+			"to": "ECE 6787"
+		},
+		{
+			"from": "ECE 6610",
+			"to": "ECE 6615"
+		},
+		{
+			"from": "ECE 3076",
+			"to": "ECE 6612"
+		},
+		{
+			"from": "ECE 4110",
+			"to": "ECE 6612"
+		},
+		{
+			"from": "ECE 6601",
+			"to": "ECE 6608"
+		},
+		{
+			"from": "ECE 3075",
+			"to": "ECE 6606"
+		},
+		{
+			"from": "ECE 6602",
+			"to": "ECE 6603"
+		},
+		{
+			"from": "ECE 3075",
+			"to": "ECE 6605"
+		},
+		{
+			"from": "ECE 6602",
+			"to": "ECE 6604"
+		},
+		{
+			"from": "ECE 3080",
+			"to": "ECE 6458"
+		},
+		{
+			"from": "ECE 3080",
+			"to": "ECE 6444"
+		},
+		{
+			"from": "ECE 3050",
+			"to": "ECE 6435"
+		},
+		{
+			"from": "ECE 4430",
+			"to": "ECE 6420"
+		},
+		{
+			"from": "ECE 4430",
+			"to": "ECE 6416"
+		},
+		{
+			"from": "ECE 4435",
+			"to": "ECE 6414"
+		},
+		{
+			"from": "ECE 4430",
+			"to": "ECE 6412"
+		},
+		{
+			"from": "ECE 4430",
+			"to": "ECE 6445"
+		},
+		{
+			"from": "ECE 4330",
+			"to": "ECE 6445"
+		},
+		{
+			"from": "ECE 3070",
+			"to": "ECE 6336"
+		},
+		{
+			"from": "ECE 3070",
+			"to": "ECE 6335"
+		},
+		{
+			"from": "ECE 6331",
+			"to": "ECE 6332"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 6330"
+		},
+		{
+			"from": "ECE 4320",
+			"to": "ECE 6323"
+		},
+		{
+			"from": "ECE 4430",
+			"to": "ECE 6422"
+		},
+		{
+			"from": "ECE 4320",
+			"to": "ECE 6322"
+		},
+		{
+			"from": "ECE 4320",
+			"to": "ECE 6321"
+		},
+		{
+			"from": "ECE 6250",
+			"to": "ECE 6283"
+		},
+		{
+			"from": "ECE 4270",
+			"to": "ECE 6282"
+		},
+		{
+			"from": "ECE 4390",
+			"to": "ECE 6282"
+		},
+		{
+			"from": "ECE 6272",
+			"to": "ECE 6282"
+		},
+		{
+			"from": "ECE 4270",
+			"to": "ECE 6279"
+		},
+		{
+			"from": "ECE 4270",
+			"to": "ECE 6277"
+		},
+		{
+			"from": "ECE 3075",
+			"to": "ECE 6274"
+		},
+		{
+			"from": "ECE 4270",
+			"to": "ECE 6273"
+		},
+		{
+			"from": "ECE 4270",
+			"to": "ECE 6272"
+		},
+		{
+			"from": "ECE 4270",
+			"to": "ECE 6271"
+		},
+		{
+			"from": "ECE 4270",
+			"to": "ECE 6260"
+		},
+		{
+			"from": "ECE 4270",
+			"to": "ECE 6255"
+		},
+		{
+			"from": "ECE 6250",
+			"to": "ECE 6255"
+		},
+		{
+			"from": "ECE 4270",
+			"to": "ECE 6254"
+		},
+		{
+			"from": "ECE 4330",
+			"to": "ECE 6331"
+		},
+		{
+			"from": "ECE 4320",
+			"to": "ECE 6320"
+		},
+		{
+			"from": "ECE 4270",
+			"to": "ECE 6276"
+		},
+		{
+			"from": "ECE 6100",
+			"to": "ECE 6101"
+		},
+		{
+			"from": "ECE 4270",
+			"to": "ECE 6258"
+		},
+		{
+			"from": "ECE 3060",
+			"to": "ECE 6140"
+		},
+		{
+			"from": "ECE 4270",
+			"to": "ECE 6250"
+		},
+		{
+			"from": "ECE 3060",
+			"to": "ECE 6130"
+		},
+		{
+			"from": "ECE 3060",
+			"to": "ECE 6132"
+		},
+		{
+			"from": "ECE 3055",
+			"to": "ECE 6100"
+		},
+		{
+			"from": "ECE 3085",
+			"to": "ECE 4761"
+		},
+		{
+			"from": "ECE 3550",
+			"to": "ECE 4761"
+		},
+		{
+			"from": "ECE 2040",
+			"to": "ECE 4782"
+		},
+		{
+			"from": "ECE 2025",
+			"to": "ECE 4783"
+		},
+		{
+			"from": "ECE 2026",
+			"to": "ECE 4783"
+		},
+		{
+			"from": "ECE 3077",
+			"to": "ECE 4783"
+		},
+		{
+			"from": "ECE 3030",
+			"to": "ECE 4754"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 4754"
+		},
+		{
+			"from": "ECE 3710",
+			"to": "ECE 4754"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 4752"
+		},
+		{
+			"from": "ECE 3710",
+			"to": "ECE 4752"
+		},
+		{
+			"from": "ECE 3043",
+			"to": "ECE 4612"
+		},
+		{
+			"from": "ECE 3030",
+			"to": "ECE 4784"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 4784"
+		},
+		{
+			"from": "ECE 3076",
+			"to": "ECE 4605"
+		},
+		{
+			"from": "ECE 3600",
+			"to": "ECE 4605"
+		},
+		{
+			"from": "ECE 3076",
+			"to": "ECE 4604"
+		},
+		{
+			"from": "ECE 3600",
+			"to": "ECE 4604"
+		},
+		{
+			"from": "ECE 4601",
+			"to": "ECE 4602"
+		},
+		{
+			"from": "ECE 3030",
+			"to": "ECE 4781"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 4781"
+		},
+		{
+			"from": "ECE 3710",
+			"to": "ECE 4781"
+		},
+		{
+			"from": "ECE 2025",
+			"to": "ECE 4580"
+		},
+		{
+			"from": "ECE 2026",
+			"to": "ECE 4580"
+		},
+		{
+			"from": "ECE 3085",
+			"to": "ECE 4570"
+		},
+		{
+			"from": "ECE 3550",
+			"to": "ECE 4570"
+		},
+		{
+			"from": "ECE 3020",
+			"to": "ECE 4601"
+		},
+		{
+			"from": "ECE 3084",
+			"to": "ECE 4601"
+		},
+		{
+			"from": "ECE 3077",
+			"to": "ECE 4601"
+		},
+		{
+			"from": "ECE 3085",
+			"to": "ECE 4562"
+		},
+		{
+			"from": "ECE 3550",
+			"to": "ECE 4562"
+		},
+		{
+			"from": "ECE 2040",
+			"to": "ECE 4563"
+		},
+		{
+			"from": "ECE 3710",
+			"to": "ECE 4563"
+		},
+		{
+			"from": "ECE 3084",
+			"to": "ECE 4555"
+		},
+		{
+			"from": "ECE 3085",
+			"to": "ECE 4555"
+		},
+		{
+			"from": "ECE 3550",
+			"to": "ECE 4555"
+		},
+		{
+			"from": "ECE 3076",
+			"to": "ECE 4607"
+		},
+		{
+			"from": "ECE 3600",
+			"to": "ECE 4607"
+		},
+		{
+			"from": "ECE 3025",
+			"to": "ECE 4502"
+		},
+		{
+			"from": "ECE 3025",
+			"to": "ECE 4501"
+		},
+		{
+			"from": "ECE 3030",
+			"to": "ECE 4460"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 4460"
+		},
+		{
+			"from": "ECE 3710",
+			"to": "ECE 4460"
+		},
+		{
+			"from": "ECE 3080",
+			"to": "ECE 4451"
+		},
+		{
+			"from": "ECE 3450",
+			"to": "ECE 4451"
+		},
+		{
+			"from": "ECE 3041",
+			"to": "ECE 4435"
+		},
+		{
+			"from": "ECE 3043",
+			"to": "ECE 4435"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 4420"
+		},
+		{
+			"from": "ECE 3030",
+			"to": "ECE 4420"
+		},
+		{
+			"from": "ECE 2025",
+			"to": "ECE 4606"
+		},
+		{
+			"from": "ECE 2026",
+			"to": "ECE 4606"
+		},
+		{
+			"from": "ECE 3020",
+			"to": "ECE 4606"
+		},
+		{
+			"from": "ECE 3084",
+			"to": "ECE 4606"
+		},
+		{
+			"from": "ECE 3077",
+			"to": "ECE 4606"
+		},
+		{
+			"from": "ECE 3025",
+			"to": "ECE 4418"
+		},
+		{
+			"from": "ECE 3400",
+			"to": "ECE 4418"
+		},
+		{
+			"from": "ECE 3025",
+			"to": "ECE 4415"
+		},
+		{
+			"from": "ECE 3050",
+			"to": "ECE 4415"
+		},
+		{
+			"from": "ECE 3400",
+			"to": "ECE 4415"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 4410"
+		},
+		{
+			"from": "ECE 3084",
+			"to": "ECE 4560"
+		},
+		{
+			"from": "ECE 3085",
+			"to": "ECE 4560"
+		},
+		{
+			"from": "ECE 3550",
+			"to": "ECE 4560"
+		},
+		{
+			"from": "ECE 3025",
+			"to": "ECE 4391"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 4391"
+		},
+		{
+			"from": "ECE 3065",
+			"to": "ECE 4390"
+		},
+		{
+			"from": "ECE 4350",
+			"to": "ECE 4390"
+		},
+		{
+			"from": "ECE 3065",
+			"to": "ECE 4360"
+		},
+		{
+			"from": "ECE 4350",
+			"to": "ECE 4360"
+		},
+		{
+			"from": "ECE 4415",
+			"to": "ECE 4360"
+		},
+		{
+			"from": "ECE 3070",
+			"to": "ECE 4335"
+		},
+		{
+			"from": "ECE 3300",
+			"to": "ECE 4335"
+		},
+		{
+			"from": "ECE 3025",
+			"to": "ECE 4500"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 4330"
+		},
+		{
+			"from": "ECE 3043",
+			"to": "ECE 4330"
+		},
+		{
+			"from": "ECE 3070",
+			"to": "ECE 4325"
+		},
+		{
+			"from": "ECE 3300",
+			"to": "ECE 4325"
+		},
+		{
+			"from": "ECE 3070",
+			"to": "ECE 4321"
+		},
+		{
+			"from": "ECE 3300",
+			"to": "ECE 4321"
+		},
+		{
+			"from": "ECE 2031",
+			"to": "ECE 4550"
+		},
+		{
+			"from": "ECE 3550",
+			"to": "ECE 4550"
+		},
+		{
+			"from": "ECE 3085",
+			"to": "ECE 4550"
+		},
+		{
+			"from": "ECE 3084",
+			"to": "ECE 4550"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 4445"
+		},
+		{
+			"from": "ECE 3741",
+			"to": "ECE 4445"
+		},
+		{
+			"from": "ECE 3084",
+			"to": "ECE 4445"
+		},
+		{
+			"from": "ECE 3043",
+			"to": "ECE 4446"
+		},
+		{
+			"from": "ECE 4445",
+			"to": "ECE 4446"
+		},
+		{
+			"from": "ECE 3030",
+			"to": "ECE 4452"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 4452"
+		},
+		{
+			"from": "ECE 2031",
+			"to": "ECE 4452"
+		},
+		{
+			"from": "ECE 3050",
+			"to": "ECE 4430"
+		},
+		{
+			"from": "ECE 3400",
+			"to": "ECE 4430"
+		},
+		{
+			"from": "ECE 4270",
+			"to": "ECE 4273"
+		},
+		{
+			"from": "ECE 4270",
+			"to": "ECE 4271"
+		},
+		{
+			"from": "ECE 3025",
+			"to": "ECE 4370"
+		},
+		{
+			"from": "ECE 2031",
+			"to": "ECE 4175"
+		},
+		{
+			"from": "ECE 2031",
+			"to": "ECE 4170"
+		},
+		{
+			"from": "ECE 3084",
+			"to": "ECE 4260"
+		},
+		{
+			"from": "ECE 3077",
+			"to": "ECE 4260"
+		},
+		{
+			"from": "ECE 2031",
+			"to": "ECE 4185"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 4185"
+		},
+		{
+			"from": "ECE 3055",
+			"to": "ECE 4185"
+		},
+		{
+			"from": "ECE 3056",
+			"to": "ECE 4185"
+		},
+		{
+			"from": "ECE 3025",
+			"to": "ECE 4350"
+		},
+		{
+			"from": "ECE 2035",
+			"to": "ECE 4122"
+		},
+		{
+			"from": "ECE 2036",
+			"to": "ECE 4122"
+		},
+		{
+			"from": "ECE 3076",
+			"to": "ECE 4112"
+		},
+		{
+			"from": "ECE 3600",
+			"to": "ECE 4112"
+		},
+		{
+			"from": "ECE 4110",
+			"to": "ECE 4112"
+		},
+		{
+			"from": "ECE 3076",
+			"to": "ECE 4110"
+		},
+		{
+			"from": "ECE 3600",
+			"to": "ECE 4110"
+		},
+		{
+			"from": "ECE 3070",
+			"to": "ECE 4320"
+		},
+		{
+			"from": "ECE 3300",
+			"to": "ECE 4320"
+		},
+		{
+			"from": "ECE 3077",
+			"to": "ECE 4270"
+		},
+		{
+			"from": "ECE 3084",
+			"to": "ECE 4270"
+		},
+		{
+			"from": "ECE 4260",
+			"to": "ECE 4270"
+		},
+		{
+			"from": "ECE 3042",
+			"to": "ECE 4007"
+		},
+		{
+			"from": "ECE 4001",
+			"to": "ECE 4007"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 4001"
+		},
+		{
+			"from": "ECE 3056",
+			"to": "ECE 4181"
+		},
+		{
+			"from": "ECE 3050",
+			"to": "ECE 4130"
+		},
+		{
+			"from": "ECE 3060",
+			"to": "ECE 4130"
+		},
+		{
+			"from": "ECE 3150",
+			"to": "ECE 4130"
+		},
+		{
+			"from": "ECE 3400",
+			"to": "ECE 4130"
+		},
+		{
+			"from": "ECE 3043",
+			"to": "ECE 4043"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 4043"
+		},
+		{
+			"from": "ECE 3055",
+			"to": "ECE 4100"
+		},
+		{
+			"from": "ECE 3056",
+			"to": "ECE 4100"
+		},
+		{
+			"from": "ECE 3035",
+			"to": "ECE 4180"
+		},
+		{
+			"from": "ECE 3055",
+			"to": "ECE 4180"
+		},
+		{
+			"from": "ECE 3056",
+			"to": "ECE 4180"
+		},
+		{
+			"from": "ECE 2031",
+			"to": "ECE 4180"
+		},
+		{
+			"from": "ECE 2035",
+			"to": "ECE 4180"
+		},
+		{
+			"from": "ECE 2036",
+			"to": "ECE 4180"
+		},
+		{
+			"from": "ECE 3035",
+			"to": "ECE 4180"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 4180"
+		},
+		{
+			"from": "ECE 3055",
+			"to": "ECE 4180"
+		},
+		{
+			"from": "ECE 3056",
+			"to": "ECE 4180"
+		},
+		{
+			"from": "ECE 4011",
+			"to": "ECE 4012"
+		},
+		{
+			"from": "ECE 3025",
+			"to": "ECE 4012"
+		},
+		{
+			"from": "ECE 3072",
+			"to": "ECE 4012"
+		},
+		{
+			"from": "ECE 3084",
+			"to": "ECE 4012"
+		},
+		{
+			"from": "ECE 3020",
+			"to": "ECE 4012"
+		},
+		{
+			"from": "ECE 3030",
+			"to": "ECE 4012"
+		},
+		{
+			"from": "ECE 3050",
+			"to": "ECE 3431"
+		},
+		{
+			"from": "ECE 3042",
+			"to": "ECE 3431"
+		},
+		{
+			"from": "ECE 2025",
+			"to": "ECE 3090"
+		},
+		{
+			"from": "ECE 2030",
+			"to": "ECE 3090"
+		},
+		{
+			"from": "ECE 2040",
+			"to": "ECE 3090"
+		},
+		{
+			"from": "ECE 2040",
+			"to": "ECE 3085"
+		},
+		{
+			"from": "ECE 2025",
+			"to": "ECE 3085"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 3080"
+		},
+		{
+			"from": "ECE 3025",
+			"to": "ECE 4011"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 4011"
+		},
+		{
+			"from": "ECE 3043",
+			"to": "ECE 4011"
+		},
+		{
+			"from": "ECE 3072",
+			"to": "ECE 4011"
+		},
+		{
+			"from": "ECE 3084",
+			"to": "ECE 4011"
+		},
+		{
+			"from": "ECE 3020",
+			"to": "ECE 4011"
+		},
+		{
+			"from": "ECE 3030",
+			"to": "ECE 4011"
+		},
+		{
+			"from": "ECE 3056",
+			"to": "ECE 4011"
+		},
+		{
+			"from": "ECE 3005",
+			"to": "ECE 4011"
+		},
+		{
+			"from": "ECE 3077",
+			"to": "ECE 4011"
+		},
+		{
+			"from": "ECE 2025",
+			"to": "ECE 3076"
+		},
+		{
+			"from": "ECE 2030",
+			"to": "ECE 3076"
+		},
+		{
+			"from": "ECE 2040",
+			"to": "ECE 3071"
+		},
+		{
+			"from": "ECE 3710",
+			"to": "ECE 3071"
+		},
+		{
+			"from": "ECE 3025",
+			"to": "ECE 3070"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 3070"
+		},
+		{
+			"from": "ECE 3025",
+			"to": "ECE 3065"
+		},
+		{
+			"from": "ECE 2025",
+			"to": "ECE 3075"
+		},
+		{
+			"from": "ECE 2031",
+			"to": "ECE 3060"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 3060"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 3450"
+		},
+		{
+			"from": "ECE 2040",
+			"to": "ECE 3550"
+		},
+		{
+			"from": "ECE 2020",
+			"to": "ECE 3600"
+		},
+		{
+			"from": "ECE 2030",
+			"to": "ECE 3600"
+		},
+		{
+			"from": "ECE 2026",
+			"to": "ECE 3600"
+		},
+		{
+			"from": "ECE 2025",
+			"to": "ECE 3600"
+		},
+		{
+			"from": "ECE 2031",
+			"to": "ECE 3055"
+		},
+		{
+			"from": "ECE 3710",
+			"to": "ECE 3741"
+		},
+		{
+			"from": "ECE 2031",
+			"to": "ECE 3150"
+		},
+		{
+			"from": "ECE 2040",
+			"to": "ECE 3150"
+		},
+		{
+			"from": "ECE 3025",
+			"to": "ECE 3300"
+		},
+		{
+			"from": "ECE 2026",
+			"to": "ECE 3084"
+		},
+		{
+			"from": "ECE 2025",
+			"to": "ECE 3084"
+		},
+		{
+			"from": "ECE 2040",
+			"to": "ECE 3084"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 3400"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 3050"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 3042"
+		},
+		{
+			"from": "ECE 3041",
+			"to": "ECE 3042"
+		},
+		{
+			"from": "ECE 2031",
+			"to": "ECE 3041"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 3041"
+		},
+		{
+			"from": "ECE 2030",
+			"to": "ECE 3035"
+		},
+		{
+			"from": "ECE 2040",
+			"to": "ECE 3072"
+		},
+		{
+			"from": "ECE 3710",
+			"to": "ECE 3072"
+		},
+		{
+			"from": "ECE 2031",
+			"to": "ECE 3056"
+		},
+		{
+			"from": "ECE 2035",
+			"to": "ECE 3056"
+		},
+		{
+			"from": "ECE 3035",
+			"to": "ECE 3056"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 3043"
+		},
+		{
+			"from": "ECE 2031",
+			"to": "ECE 3043"
+		},
+		{
+			"from": "ECE 2020",
+			"to": "ECE 3030"
+		},
+		{
+			"from": "ECE 2030",
+			"to": "ECE 3030"
+		},
+		{
+			"from": "ECE 2040",
+			"to": "ECE 3030"
+		},
+		{
+			"from": "ECE 2031",
+			"to": "ECE 3005"
+		},
+		{
+			"from": "ECE 3025",
+			"to": "ECE 3005"
+		},
+		{
+			"from": "ECE 3040",
+			"to": "ECE 3005"
+		},
+		{
+			"from": "ECE 3072",
+			"to": "ECE 3005"
+		},
+		{
+			"from": "ECE 3084",
+			"to": "ECE 3005"
+		},
+		{
+			"from": "ECE 3020",
+			"to": "ECE 3005"
+		},
+		{
+			"from": "ECE 3030",
+			"to": "ECE 3005"
+		},
+		{
+			"from": "ECE 3056",
+			"to": "ECE 3005"
+		},
+		{
+			"from": "ECE 2031",
+			"to": "ECE 3006"
+		},
+		{
+			"from": "ECE 2035",
+			"to": "ECE 3020"
+		},
+		{
+			"from": "ECE 2036",
+			"to": "ECE 3020"
+		},
+		{
+			"from": "ECE 3043",
+			"to": "ECE 3040"
+		},
+		{
+			"from": "ECE 3041",
+			"to": "ECE 3040"
+		},
+		{
+			"from": "ECE 2031",
+			"to": "ECE 3040"
+		},
+		{
+			"from": "ECE 2035",
+			"to": "ECE 3040"
+		},
+		{
+			"from": "ECE 2036",
+			"to": "ECE 3040"
+		},
+		{
+			"from": "ECE 2040",
+			"to": "ECE 3040"
+		},
+		{
+			"from": "ECE 2040",
+			"to": "ECE 3025"
+		},
+		{
+			"from": "ECE 3710",
+			"to": "ECE 3025"
+		},
+		{
+			"from": "ECE 2025",
+			"to": "ECE 3025"
+		},
+		{
+			"from": "ECE 2026",
+			"to": "ECE 3025"
+		}
+	];
---
+++ jsonlab-1.1/nodeGen.m
@@ -0,0 +1,31 @@
+%% nodeGen: generates the nodes based on a Grouch json file
+
+function nodeGen(fname, outname)
+    data = loadjson(fname);
+    ind = [];
+    for x = 1:length(data)
+        st = data{x};
+        [~, num] = strtok(data{x}.identifier);
+        if any(contains(data{x}.fullname,{'Special Topics', 'Special Problems', 'Undergrad'})) ...
+            || ~isfield(data{x}, 'sections') ...
+            || any(num == 'X') ...
+            || (isfield(data{x}, 'restrictions') && isfield(data{x}.restrictions, 'Campuses') && ~contains(data{x}.restrictions.Campuses.requirements, 'Atlanta'))
+            ind = [ind, x];
+        end
+    end
+    fhin = fopen(fname);
+    fhout = fopen(outname, 'w');
+    fprintf(fhout, 'var nodes = ');
+    line = fgets(fhin);
+    linenum = 0;
+    while ischar(line)
+        if all(ind-linenum ~= 0) 
+            fprintf(fhout, line);
+        end
+        linenum = linenum + 1;
+        line = fgets(fhin);
+    end
+    fprintf(fhout, ';');
+    fclose(fhin);
+    fclose(fhout);
+end
